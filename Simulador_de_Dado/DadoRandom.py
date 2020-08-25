fe8daf0a6918b213316364375dd97838f32f90ac
lordefps <lodizinho_mice@hotmail.com>
2020-08-25 16:26:52 -0300

    Fazendo Menu

---
+++ Simulador_de_Dado/DadoRandom.py
@@ -0,0 +1,27 @@
+# Faz um Simulador de dado
+
+
+# import random
+
+# dado = random.randint(1, 6)
+
+# print(dado)
+
+
+
+print('''
+------ Gerar um Numero aleatório ------
+------      Escolha um dado      ------
+------      (1) - Dado de 4      ------
+------      (2) - Dado de 6      ------
+------      (3) - Dado de 8      ------
+------      (4) - Dado de 10     ------
+------      (5) - Dado de 12     ------
+------      (6) - Dado de 20     ------
+------      (6) - Dado de 100    ------
+---------------------------------------
+---------------------------------------
+
+'''
+)
+
